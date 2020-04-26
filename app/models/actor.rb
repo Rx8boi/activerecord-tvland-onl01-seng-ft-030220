efa72d23f20f416e4110dd524d22af36b96dfaa5
Charles Butler <cdbutle@outlook.com>
2020-04-26 19:35:02 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,8 +3,6 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    self.first_name = a
-    self.last_name = b
-    ab
+    "#{self.first_name self.last_name}""
   end
 end

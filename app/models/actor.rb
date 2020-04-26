22a8daaaf89526110f0d314096ddd6202893043b
Charles Butler <cdbutle@outlook.com>
2020-04-26 19:51:30 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,6 @@
 class Actor < ActiveRecord::Base
   has_many :characters
-  has_many :show, through: :characters
+  has_many :shows, through: :characters
 
   def full_name
     self.first_name+(" ")+self.last_name
@@ -9,7 +9,7 @@ class Actor < ActiveRecord::Base
   def list_roles
     roles = []
        array_of_characters = self.characters.map {|character| character.name}
-       array_of_shows = self.show.map {|shows| shows.name}
+       array_of_shows = self.shows.map {|shows| shows.name}
        role = array_of_characters.concat array_of_shows
        roles << role.join(" - ")
        roles

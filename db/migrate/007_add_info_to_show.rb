f654a674da0959e583371817111ed685a77b53a7
Charles Butler <cdbutle@outlook.com>
2020-04-26 20:02:43 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_info_to_show.rb
+++ db/migrate/007_add_info_to_show.rb
@@ -5,5 +5,6 @@ class AddInfoToShow < ActiveRecord::Migration[5.1]
   def change
     add_column :show, :day, :string
     add_column :show, :genre, :string
+    add_column :show, :season, :string
   end
 end

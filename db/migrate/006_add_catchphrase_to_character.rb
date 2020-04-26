f81b6406d131e43dd373d686a7d097980f12a6b7
Charles Butler <cdbutle@outlook.com>
2020-04-26 19:28:55 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_character.rb
+++ db/migrate/006_add_catchphrase_to_character.rb
@@ -3,6 +3,6 @@ class AddCatchphraseToCharacter < ActiveRecord::Migration[5.1]
   #a new migration, and then in the change method,
   #we make the change.
   def change
-    add_column :catchphrase, :character_id, :string
+    add_column :character, :catchphrase, :string
   end
 end

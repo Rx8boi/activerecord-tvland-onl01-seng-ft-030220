e91078e8d6faf678e7782e4ffedc9ccf545279d6
Charles Butler <cdbutle@outlook.com>
2020-04-26 19:24:28 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,7 +1,7 @@
 class CreateCharacters < ActiveRecord::Migration[5.1]
   #define a change method in which to do the migration
   def change
-    create_table :characters do |t| 
+    create_table :characters do |t|
       t.integer :actor_id
       t.integer :show_id
     end
